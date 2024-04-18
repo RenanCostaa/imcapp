f5d71c7530af69f3355666bab07103a7d9ad7b5e
renan <renancosta@ucl.br>
2024-04-18 19:34:37 -0300

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class HomeIMC extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _HomeIMCState extends State<HomeIMC> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
