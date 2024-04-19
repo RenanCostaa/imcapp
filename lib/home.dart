faa5911969b01ad5333f63c43e6d449905fa7909
renan <renancosta@ucl.br>
2024-04-18 21:47:06 -0300

    Inserindo controller

--- lib/home.dart
+++ lib/home.dart
@@ -8,53 +8,57 @@ class HomeIMC extends StatefulWidget {
 }
 
 class HomeIMCState extends State<HomeIMC> {
-  TextEditingController _alturaController = TextEditingController();
-  TextEditingController _pesoController = TextEditingController();
+  final TextEditingController _alturaController = TextEditingController();
+  final TextEditingController _pesoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('IMC - Tabajara Calculator'),
-        actions: [
-          IconButton(
-              icon: const Icon(Icons.replay_outlined), onPressed: _limpaCampos)
-        ],
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-              padding: const EdgeInsets.all(20),
-              child: Image.asset(
-                'images/weight.jpg',
-                height: 200,
-              )),
-          Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                controller: _alturaController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Altura'),
-              )),
-          Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                  controller: _pesoController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: 'Peso'))),
-          Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20)),
-                onPressed: _calculaIMC,
-                child: const Text('Calcular IMC'),
-              ))
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: const Text('IMC - Tabajara Calculator'),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.replay_outlined),
+                onPressed: _limpaCampos)
+          ],
+        ),
+        body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/background.jpg"),
+                    fit: BoxFit.cover)),
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Image.asset(
+                        'images/weight.jpg',
+                        height: 200,
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        controller: _alturaController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(labelText: 'Altura'),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                          controller: _pesoController,
+                          keyboardType: TextInputType.number,
+                          decoration:
+                              const InputDecoration(labelText: 'Peso'))),
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              textStyle: const TextStyle(fontSize: 20)),
+                          onPressed: _calculaIMC,
+                          child: const Text('Calcular IMC')))
+                ]))));
   }
 
   void _limpaCampos() {
@@ -62,5 +66,8 @@ class HomeIMCState extends State<HomeIMC> {
     _pesoController.text = "";
   }
 
-  _calculaIMC() {}
+  _calculaIMC() {
+    double alt = double.parse(_alturaController.text);
+    double pes = double.parse(_pesoController.text);
+  }
 }
