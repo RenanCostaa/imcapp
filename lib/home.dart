65331c5ce16fa2ac3fa3535aae719b31d02b2afe
RenanCostaa <renancosta@ucl.br>
2024-04-25 22:01:31 -0300

    Abonitando

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:imcapp/imc.dart';
 import 'package:imcapp/resultado.dart';
 
@@ -35,32 +36,54 @@ class HomeIMCState extends State<HomeIMC> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                   Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Image.asset(
-                        'images/weight.jpg',
-                        height: 200,
-                      )),
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 2),
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'images/weight.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
                   Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: TextField(
-                        controller: _alturaController,
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(labelText: 'Altura'),
-                      )),
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      controller: _alturaController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')), // Permite apenas números
+                      ],
+                      decoration: const InputDecoration(labelText: 'Altura'),
+                    ),
+                  ),
                   Padding(
                       padding: const EdgeInsets.all(20.0),
                       child: TextField(
                           controller: _pesoController,
                           keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                        ],
                           decoration:
                               const InputDecoration(labelText: 'Peso'))),
                   Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 20)),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.red), 
+                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                            minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+                            padding: MaterialStateProperty.all(const EdgeInsets.all(20.0))
+                            ),
                           onPressed: _calculaIMC,
-                          child: const Text('Calcular IMC')))
+                          child: const Text('Calcular IMC', style: TextStyle(fontSize: 16))))
                 ]))));
   }
 
